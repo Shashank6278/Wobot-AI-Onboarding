5608213a5e592f63b096934adca434bde2ef43ee
Shashank6278 <shashankraju123@gmail.com>
2026-03-13 12:58:36 +0530

    gst parse launch for GSTREAMER PIPELINE

--- app/reproxy.py
+++ app/reproxy.py
@@ -1,5 +1,22 @@
-import subprocess
+import gi
+gi.require_version("Gst", "1.0")
+from gi.repository import Gst
 
-pipeline = "gst-launch-1.0 rtspsrc location=rtsp://localhost:8554/input protocols=tcp latency=0 ! rtph264depay ! decodebin ! x264enc bitrate=2048 ! rtspclientsink location=rtsp://localhost:8554/output protocols=tcp"
+Gst.init(None)
 
-subprocess.run(pipeline, shell=True)
+pipeline_str = """
+rtspsrc location=rtsp://localhost:8554/input protocols=tcp latency=0 !
+rtph264depay !
+decodebin !
+x264enc bitrate=2048 !
+rtspclientsink location=rtsp://localhost:8554/output protocols=tcp
+"""
+
+pipeline = Gst.parse_launch(pipeline_str)
+
+pipeline.set_state(Gst.State.PLAYING)
+
+try:
+    input("Pipeline running... Press Enter to stop\n")
+finally:
+    pipeline.set_state(Gst.State.NULL)
